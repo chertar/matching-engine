f78d84b65004ed595866c5fac153cdc4d3cf282f
Tarik Cherkaoui <chertar@gmail.com>
2021-06-05 16:10:27 -0400

    Added tests

--- src/test/java/com/chertar/BookSideTest.java
+++ src/test/java/com/chertar/BookSideTest.java
@@ -10,25 +10,56 @@ import static org.assertj.core.util.Lists.list;
 
 public class BookSideTest extends TestCase {
 
-    public void testPostOrder() {
-        testPermutation(list(
+    public void testPostingBuyOrders() {
+        testPermutation(Side.BUY, list(
                 limit(Side.BUY, 100, 100.25)),
                 sidedQuote(100.25, 100));
 
-        testPermutation(list(
+        testPermutation(Side.SELL, list(
                 limit(Side.BUY, 50, 100.25),
                 limit(Side.BUY, 100, 100.25)),
                 sidedQuote(100.25, 150));
 
-        testPermutation(list(
+        testPermutation(Side.BUY, list(
                 limit(Side.BUY, 50, 100.25),
                 limit(Side.BUY, 100, 100.25),
                 limit(Side.BUY, 25, 101.0)),
                 sidedQuote(101.0, 25));
+
+        testPermutation(Side.BUY, list(
+                limit(Side.BUY, 50, 100.25),
+                limit(Side.BUY, 100, 100.25),
+                limit(Side.BUY, 25, 101.0),
+                limit(Side.BUY, 10, 99.0)),
+                sidedQuote(101.0, 25));
     }
 
-    private static void testPermutation(List<Order> orders, BookSide.SidedQuote expectedQuote) {
-        BookSide bookSide = new BookSide(Side.BUY);
+    public void testPostingSellOrders() {
+        testPermutation(Side.SELL, list(
+                limit(Side.SELL, 100, 100.25)),
+                sidedQuote(100.25, 100));
+
+        testPermutation(Side.SELL, list(
+                limit(Side.SELL, 50, 100.25),
+                limit(Side.SELL, 100, 100.25)),
+                sidedQuote(100.25, 150));
+
+        testPermutation(Side.SELL, list(
+                limit(Side.SELL, 50, 100.25),
+                limit(Side.SELL, 100, 100.25),
+                limit(Side.SELL, 25, 101.0)),
+                sidedQuote(100.25, 150));
+
+        testPermutation(Side.SELL, list(
+                limit(Side.SELL, 50, 100.25),
+                limit(Side.SELL, 100, 100.25),
+                limit(Side.SELL, 25, 101.0),
+                limit(Side.SELL, 10, 99.0)),
+                sidedQuote(99.0, 10));
+    }
+
+    private static void testPermutation(Side side, List<Order> orders, BookSide.SidedQuote expectedQuote) {
+        BookSide bookSide = new BookSide(side);
         for (Order order : orders) {
             bookSide.postOrder(order);
         }
