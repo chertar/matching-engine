85e90c73e775e20c24ac14676050b66226a50ef8
Tarik Cherkaoui <chertar@gmail.com>
2021-06-09 16:57:07 -0400

    Added order cache

---
+++ src/main/java/com/chertar/OrderCache.java
@@ -0,0 +1,36 @@
+package com.chertar;
+
+import com.chertar.util.MatchingEngineException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A cache which allows orders to be looked up by id in constant time
+ */
+public class OrderLookup {
+    private final Map<String, Order> orders = new HashMap<>();
+
+    public Order get(String id) {
+        return orders.get(id);
+    }
+    public void add(Order order){
+        Order prexisting = orders.put(order.id(), order);
+        if (prexisting != null) {
+            if (order == prexisting) {
+                throw new MatchingEngineException("This order is already in the cache");
+            }
+            throw new MatchingEngineException("There is a prexisting order with this id");
+        }
+    }
+    // In the current implementation, orders never get removed from the map
+    // which overtime results in a memory leak.  In a production implementation
+    // I would remove orders when they are fully filled or cancelled
+    public Order remove(String id) {
+        Order order = get(id);
+        if (order == null) {
+            throw new MatchingEngineException("No order found with id '"+id+"'");
+        }
+        return orders.remove(order);
+    }
+}
